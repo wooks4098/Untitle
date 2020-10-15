910a9e1c9c47a4a1143588aec1c863b80061c996
wooks4098 <70507138+wooks4098@users.noreply.github.com>
2020-10-15 12:54:19 +0900

    초기파일업로드

---
+++ Assets/Scripts/Test/T_Bac.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class T_Bac : MonoBehaviour
+{
+    public MeshRenderer render;
+    private float offset;
+    public float Speed;
+    private void Start()
+    {
+        Speed = 0;
+
+    }
+    private void Update()
+    {
+        if (Speed <= 0.2)
+            Speed += Time.deltaTime * 0.02f;
+        offset += Time.deltaTime * Speed;
+        render.material.mainTextureOffset = new Vector2(offset, 0);
+    }
+}
---
+++ Assets/Scripts/Test/T_Obj.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class T_Obj : MonoBehaviour
+{
+    public float Speed;
+    public GameObject T_object;
+
+    void Update()
+    {
+        if (Speed <= 8.5)
+            Speed += Time.deltaTime * 1f;
+        this.transform.Translate(new Vector3(-Speed * Time.deltaTime, 0, 0));
+
+
+
+    }
+}
