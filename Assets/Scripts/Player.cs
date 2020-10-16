d3407b4a5f725b038f04caaec123e75b92a128f9
wooks4098 <70507138+wooks4098@users.noreply.github.com>
2020-10-16 21:34:47 +0900

    점프

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public Transform PlayFromCheck_Transform;//플랫폼 체크 위치
+    public float PlatFormCheck_Distance;//플랫폼 체크 길이
+    public LayerMask PlatFormCheck_Layer;//플랫폼 체크 레이어
+    public float JumpScale; //점프 크기
+    private bool isJump;//점프 가능한지 확인하는 변수 True면 가능
 
-    public float JumpScale;
-    Rigidbody2D rigid;
-    
+    Rigidbody2D rigid; //물리
 
     private void Awake()
     {
@@ -23,7 +26,20 @@ public class Player : MonoBehaviour
     }
     void Jump()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && !Input.GetKey(KeyCode.DownArrow))
+        isJump = Physics2D.Raycast(PlayFromCheck_Transform.position, Vector2.down, PlatFormCheck_Distance, PlatFormCheck_Layer);
+        Debug.DrawRay(PlayFromCheck_Transform.transform.position, Vector2.down * PlatFormCheck_Distance, Color.red, 1f);
+
+        if (Input.GetKeyDown(KeyCode.Space) && !Input.GetKey(KeyCode.DownArrow) && isJump == true)
+        {
             rigid.AddForce(Vector2.up * JumpScale, ForceMode2D.Impulse);
+            isJump = false;
+        }
+
     }
+
+    //private void OnCollisionEnter2D(Collision2D collision)
+    //{
+    //    if (collision.gameObject.tag == "PlatForm")
+    //        isJump = true;
+    //}
 }
